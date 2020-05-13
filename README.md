b1bca1e8462ca7920cc34083bc512e591e3934ee
ncaptier <60263988+ncaptier@users.noreply.github.com>
2020-05-13 19:38:57 +0200

    Create repository

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Stabilized_ICA
- This repository propose a python implementation of a stabilized ICA algorithm (ICASSO method)
+
+This repository proposes a python implementation for stabilized ICA decomposition algorithm. Most of the technical details can be found in the references [1], [2] and [3]
+
+
+## Experiments
+
+We provide a jupyter notebook for an illustration with transcriptomic data:
+* [ICA decomposition with stabilized ICA](transcriptomic_ICA.ipynb)
+
+## Requirements
+
+To run this algorithm as well as the jupyter notebook, one will need the following python packages:
+* numpy
+* matplotlib.pyplot
+* pandas
+* scikit-learn
+* tdqm 
+
+## References
+
+[1] "Determining the optimal number of independent components for reproducible transcriptomic data analysis" - Kairov et al. 2017   
+[2] "Assessing reproducibility of matrix factorization methods in independent transcriptomes" - Cantini et al. 2019
+[3] "Icasso: software for investigating the reliability of ICA estimates by clustering and visualization" - Himberg et al. 2003
---
+++ stabilized_ICA.py
@@ -0,0 +1,244 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import FastICA
+from sklearn.cluster import AgglomerativeClustering
+from sklearn import manifold
+from tqdm import tqdm
+
+def ICA_decomposition(X , n_components, max_iter):
+    """ Apply FastICA algorithm from sklearn.decompostion to the matrix X
+        
+        Note: FastICA in sklearn works with a matrix of shape (n_features , n_samples)
+              that is why we fit FastICA with X.T
+              
+    Parameters
+    ----------
+    X : 2D array, shape (n_samples , n_features)
+
+    n_components : int
+        number of ICA components
+        
+    max_iter : int
+        see https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.FastICA.html
+
+    Returns
+    -------
+    2D array , shape (n_components , n_features)
+        components obtained from the ICA decomposition of X
+
+    """
+    ica = FastICA(n_components = n_components , max_iter = max_iter)
+    ica.fit(X.T)
+    return ica.transform(X.T).T
+
+def centrotype(X , S , cluster_labels):
+    """Compute the centrotype of the cluster of ICA components defined by cluster_labels
+    
+       centrotype: component of the cluster which is the most similar to the other components
+                   of the cluster
+    Parameters
+    ----------
+    X : 2D array, shape (n_components , n_features)
+        matrix of independent ICA components
+        
+    S : 2D array, shape (n_components , n_components)
+        similarity Matrix for ICA components (i.e rows of X)
+        
+    cluster_labels : list of integers
+        indexes of a cluster of components (ex:[0 , 1 , 7] refers to the rows 0, 1 and 7 of X)
+
+    Returns
+    -------
+    1D array, shape ( , n_features)
+        centrotype of the cluster of ICA components defined by cluster_labels
+
+    """
+    temp = np.argmax(np.sum(S[np.ix_(cluster_labels , cluster_labels)] , axis=0))
+    return X[cluster_labels[temp] , :]
+
+def stability_index(S , cluster_labels):
+    """Compute the stability index for a cluster of ICA components.
+        
+       Note: Please refer to https://bmcgenomics.biomedcentral.com/track/pdf/10.1186/s12864-017-4112-9
+             (section "Method") for the exact formula of the stability index.
+
+    Parameters
+    ----------
+    S : 2D array, shape (n_components , n_components)
+        similarity Matrix for ICA components 
+        
+    cluster_labels : list of integers
+        indexes of a cluster of components (ex:[0 , 1 , 7] refers to the rows 0, 1 and 7 of X)
+
+    Returns
+    -------
+    Float between 0 and 1
+        stability index for the cluster of ICA components defined by cluster_labels
+
+    """
+    temp = S[np.ix_(cluster_labels , cluster_labels)]
+    ex_cluster = list(set(range(S.shape[1])) - set(cluster_labels))
+    
+    #aics = average intra-cluster similarities
+    aics = (1/len(cluster_labels)**2)*np.sum(temp)  
+    
+    #aecs = average extra-cluster similarities
+    aecs = (1/(len(ex_cluster)*len(cluster_labels)))*np.sum(S[np.ix_(cluster_labels , ex_cluster)]) 
+    
+    return aics - aecs
+
+class StabilizedICA(object):
+    
+    def __init__(self , n_components , max_iter):
+        
+        self.n_components = n_components
+        self.max_iter = max_iter
+        
+        self.S = None  #Similarity matrix between all ICA components
+        self.clusters = None
+    
+    def fit(self, X ,  n_runs , plot = False):
+        """1. Compute the ICA components of X (n = self.n_components) n_runs times
+           2. Cluster all the components (N = self.n_components*n_runs) with agglomerative 
+              agglomerative hierarchical clustering (average linkage) into self.n_components 
+              clusters
+           3. For each cluster compute its stability index and return its centrotype as the
+              final ICA components
+              
+           Note: Please refer to ICASSO method for more details about the process
+                 (see https://www.cs.helsinki.fi/u/ahyvarin/papers/Himberg03.pdf)
+                 
+        Parameters
+        ----------
+        X : 2D array, shape (n_samples , n_features)
+            
+        n_runs : int
+            number of times we run the FastICA algorithm
+            
+        plot : boolean, optional
+            if True plot the stability indexes for each cluster in decreasing order. 
+            The default is False.
+
+        Returns
+        -------
+        Index : 1D array, shape (n_components)
+            stability indexes for the n_components clusters
+            
+        Centrotypes : 2D array, shape (n_components, n_features)
+            final ICA components (i.e centrotypes of each cluster)
+
+        """
+        ## Initialisation
+        n_samples , n_features = X.shape
+        Components = np.zeros((n_runs*self.n_components , n_features))
+        Centrotypes = np.zeros((self.n_components , n_features))
+        Index = np.zeros(self.n_components)
+        
+        ## Compute the self.n_components*n_runs ICA components and store into array Components
+        for i in range(n_runs):
+            Components[i*self.n_components : (i+1)*self.n_components , : ] = ICA_decomposition(X , self.n_components , self.max_iter)
+        
+        ## Compute Similarity matrix between ICA components (Pearson correlation)
+        self.S = np.abs(np.corrcoef(x=Components , rowvar=True))
+        
+        ## Cluster the components with hierarchical clustering
+        clustering = AgglomerativeClustering(n_clusters = self.n_components , affinity = "precomputed" 
+                            ,linkage = 'average' ).fit(1 - self.S)
+        self.clusters = clustering.labels_ 
+        
+        ## For each cluster compute the stability index and the centrotype
+        for i in range(self.n_components):
+            cluster_labels = list(np.argwhere(clustering.labels_ == i ).flatten())
+            Centrotypes[i , :] = centrotype(Components , self.S , cluster_labels)
+            Index[i] = stability_index(self.S , cluster_labels)
+        
+        ## Sort the centrotypes (i.e final components) by stability index
+        indices = np.argsort(-1*Index)
+        Centrotypes , Index = Centrotypes[indices , :] , Index[indices]
+        
+        if plot:
+            plt.figure(figsize=(10 , 7))
+            plt.plot(range(1 , self.n_components + 1) , Index , linestyle='--', marker='o')
+            plt.title("Stability of ICA components")
+            plt.xlabel("ICA components")
+            plt.ylabel("Stability index")
+            
+        return Index, Centrotypes
+    
+    def projection(self , ax = None):
+        """Plot the ICA components computed during fit() (N = self.n_components*n_runs) in 2D.
+           Approximate the original dissimilarities between components by Euclidean distance.
+           Each cluster is represented with a different color.
+           
+           Note: We use the dissimilarity measure sqrt(1 - |rho_ij|) (rho the Pearson correlation)
+                 instead of 1 - |rho_ij| to reduce overlapping.
+        
+        Parameters
+        ----------
+        
+        ax : matplotlib.axes
+            
+        Returns
+        -------
+        None.
+
+        """
+        embedding = manifold.MDS(n_components=2, dissimilarity= "precomputed", random_state=6)
+        P = embedding.fit_transform(np.sqrt(1 - self.S))
+        if ax is None:
+            plt.scatter(P[: , 0] , P[: , 1] , c=self.clusters , cmap = 'viridis')
+            plt.title("")
+        else:
+            ax.scatter(P[: , 0] , P[: , 1] , c=self.clusters , cmap = 'viridis')
+        return
+    
+    
+def MSTD(X , m , M , step , n_runs , max_iter = 2000):
+    """Plot "MSTD graphs" to help choosing an optimal dimension for ICA decomposition
+        
+       Run stabilized ICA algorithm for several dimensions in [m , M] and compute the
+       stability distribution of the components each time
+       
+       Note: Please refer to https://bmcgenomics.biomedcentral.com/track/pdf/10.1186/s12864-017-4112-9
+             for more details.
+
+    Parameters
+    ----------
+    X : 2D array, shape (n_samples , n_features)
+    
+    m : int
+        minimal dimension for ICA decomposition
+        
+    M : int > m
+        maximal dimension for ICA decomposition
+        
+    step : int > 0
+        step between two dimensions (ex: if step = 2 the function will test the dimensions
+        m, m+2, m+4, ... , M)
+        
+    n_runs : int
+        number of times we run the FastICA algorithm (see fit method of class Stabilized_ICA)
+            
+    max_iter : TYPE, optional
+        parameter for ICA_decomposition. The default is 2000.
+
+    Returns
+    -------
+    None.
+
+    """
+    fig, ax = plt.subplots(1 , 2 , figsize = (20 , 7))
+    mean = []
+    for i in tqdm(range(m , M+step , step)):
+        s = StabilizedICA(i , max_iter)
+        Index,*_ = s.fit(X , n_runs)
+        mean.append(np.mean(Index))
+        ax[0].plot(range(1 , len(Index)+1) , Index , 'k')
+        
+    ax[1].plot(range(m , M+step , step) , mean) 
+    
+    ax[1].set_title("Mean stability")
+    ax[1].set_xlabel("Number of components")
+    ax[0].set_title("Index stability distribution")
+    ax[0].set_xlabel("Number of components")    
+    return 

8d30a79e563c2e8e30f5531c399c0c6556507895
ncaptier <60263988+ncaptier@users.noreply.github.com>
2020-11-27 12:58:42 +0100

    Update stabilized ICA
    - Externalise the PCA step to speed up computations
    - Change the input shape (add commnets to clarify)
    - Add new arguments like func and algorithm

--- algorithms/stabilized_ICA.py
+++ algorithms/stabilized_ICA.py
@@ -1,38 +1,66 @@
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
-from sklearn.decomposition import FastICA
+from sklearn.decomposition import FastICA , PCA
 from sklearn.cluster import AgglomerativeClustering
 from sklearn import manifold
 import umap
 from tqdm.notebook import tqdm
 from joblib import Parallel, delayed
 
-def _ICA_decomposition(X , n_components, max_iter):
+"""
+Introduction
+------------
+
+Given an input matrix X (n_observations x n_variables) the ICA algorithm decomposes X into :
+    X = S*A  with S (n_observations x n_variables) and A (n_variables x n_variables)
+where the columns of S are independent.
+
+In practice, we are often interested in dimension reduction and we can thus set a reduced number 
+of components such that :
+    X ~ S*A with S (n_observations x n_components) and A (n_components x n_variables)
+where the columns of S are independent.
+
+In signal processing the problem of blind source separation can be solved by ICA with and input
+matrix X (observations = times of registration x variables = initial signals).
+
+In gene expression study, we typically want to unravel independent biolgical sources. To do so,
+we can apply ICA on a matrix X (observations = genes x variables = biological samples).
+
+Important note
+--------------
+PLEASE NOTE THAT IN THE FOLLOWING THE MATRICES S AND A WILL BE GIVEN IN THEIR TRANSPOSED SHAPE
+SIMPLY BY CONVENTION.
+"""
+
+def _ICA_decomposition(X , fun , algorithm , max_iter):
     """ Apply FastICA algorithm from sklearn.decompostion to the matrix X
-        
-        Note : FastICA in sklearn works with a matrix of shape (n_features , n_samples)
-              that is why we fit FastICA with X.T
-              
+               
     Parameters
     ----------
-    X : 2D array, shape (n_samples , n_features)
-
-    n_components : int
-        number of ICA components
+    X : 2D array, shape (n_observations , n_components) 
+        Whitened matrix.
+        
+    fun : string or function.
+        The functional form of the G function used in the approximation to neg-entropy. Could be either ‘logcosh’, ‘exp’
+        , or ‘cube’. You can also provide your own function. It should return a tuple containing the value of the function, 
+        and of its derivative, in the point.
+        
+    algorithm : 'parallel' or 'deflation'
+        Apply parallel or deflational algorithm for FastICA
         
     max_iter : int
         see https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.FastICA.html
 
     Returns
     -------
-    2D array , shape (n_components , n_features)
+    2D array , shape (n_components , n_observations)
         components obtained from the ICA decomposition of X
 
     """
-    ica = FastICA(n_components = n_components , max_iter = max_iter)
-    ica.fit(X.T)
-    return ica.transform(X.T).T
+    ica = FastICA(fun = fun , algorithm = algorithm , max_iter = max_iter , whiten = False)
+    return ica.fit_transform(X).T
+
 
 def _centrotype(X , Sim , cluster_labels):
     """Compute the centrotype of the cluster of ICA components defined by cluster_labels
@@ -41,7 +69,7 @@ def _centrotype(X , Sim , cluster_labels):
                    of the cluster
     Parameters
     ----------
-    X : 2D array, shape (n_components , n_features)
+    X : 2D array, shape (n_components , n_observations)
         matrix of independent ICA components
         
     Sim : 2D array, shape (n_components , n_components)
@@ -52,7 +80,7 @@ def _centrotype(X , Sim , cluster_labels):
 
     Returns
     -------
-    1D array, shape (n_features)
+    1D array, shape (n_observations)
         centrotype of the cluster of ICA components defined by cluster_labels
 
     """
@@ -111,11 +139,11 @@ class StabilizedICA(object):
     Attributes
     ----------
 
-    S_: 2D array, shape (n_components , n_features)
+    S_: 2D array, shape (n_components , n_observations)
         array of sources/metagenes, each line corresponds to a stabilized ICA component (i.e the centrotype of
         a cluster of components)   
         
-    A_: 2D array, shape (n_samples , n_components)
+    A_: 2D array, shape (n_variables , n_components)
         pseudo-inverse of S_, each column corresponds to a metasample
     
     stability_indexes_ : 1D array, shape (n_components)
@@ -139,7 +167,7 @@ class StabilizedICA(object):
         self.A_ = None
         self.stability_indexes_ = None 
 
-    def fit(self, X ,  n_runs , plot = False , normalize = True , reorientation = True):
+    def fit(self, X ,  n_runs , fun = 'logcosh' , algorithm = 'parallel' , plot = False , normalize = True , reorientation = True , whiten = True):
         """1. Compute the ICA components of X n_runs times
            2. Cluster all the components (N = self.n_components*n_runs) with agglomerative 
               hierarchical clustering (average linkage) into self.n_components clusters
@@ -151,10 +179,20 @@ class StabilizedICA(object):
                  
         Parameters
         ----------
-        X : 2D array, shape (n_samples , n_features)
+        X : 2D array, shape (n_observations , n_variables) or (n_observations , n_components) if whiten is False.
             
         n_runs : int
             number of times we run the FastICA algorithm
+        
+        fun : string or function, optional.
+            The functional form of the G function used in the approximation to neg-entropy. Could be either ‘logcosh’, ‘exp’
+            , or ‘cube’. You can also provide your own function. It should return a tuple containing the value of the function, 
+            and of its derivative, in the point.
+            The default is 'logcosh'.
+            
+        algorithm : 'parallel' or 'deflation', optional.
+            Apply parallel or deflational algorithm for FastICA
+            The default is 'parallel'.
             
         plot : boolean, optional
             if True plot the stability indexes for each cluster in decreasing order. 
@@ -166,21 +204,36 @@ class StabilizedICA(object):
             
         reorientation : boolean,optional
             if True re-oriente the rows of S_ towards positive heavy tail.
-            Ther default is True.
+            The default is True.
+        
+        whiten : boolean, optional
+            if True the matrix X is whitened, i.e centered then projected in the space defined by its 
+            first self.n_components PCA components and reduced to unit variance along each of these axis. 
+            If False the input X matrix must be already whitened.
+            The default is True.
             
         Returns
         -------        
         None.
         
+        Note
+        ------        
+        If whiten is False, we suppose that X results from a whitening pre-processing step. The columns must be
+        centered, scaled to unit variance and uncorrelated.
         """
         ## Initialisation
-        n_samples , n_features = X.shape
-        Centrotypes = np.zeros((self.n_components , n_features))
+        n_observations , n_variables = X.shape
+        Centrotypes = np.zeros((self.n_components , n_observations))
         Index = np.zeros(self.n_components)
         
+        ## Pre-processing (whitening)
+        if whiten :
+            pca = PCA(n_components = self.n_components , whiten=True)
+            X_w = pca.fit_transform(X)
+            
         ## Compute the self.n_components*n_runs ICA components and store into array Components
         parallel = Parallel(n_jobs=self.n_jobs, verbose=self.verbose)        
-        decomposition = parallel(delayed(_ICA_decomposition)(X , self.n_components , self.max_iter)
+        decomposition = parallel(delayed(_ICA_decomposition)(X_w , fun , algorithm , self.max_iter)
                                  for _ in range(n_runs))
         self._Components = np.vstack(decomposition)
         
@@ -202,7 +255,7 @@ class StabilizedICA(object):
         indices = np.argsort(-1*Index)
         Centrotypes , Index = Centrotypes[indices , :] , Index[indices]
         
-        # Re-orient the stabilized ICA components towards positive heaviest tails
+        # Re-oriente the stabilized ICA components towards positive heaviest tails
         if reorientation : 
             self.S_ = (np.where(stats.skew(Centrotypes , axis = 1) >= 0 , 1 , -1).reshape(-1 , 1))*Centrotypes
         else : 
@@ -213,7 +266,7 @@ class StabilizedICA(object):
             self.S_ = self.S_/(np.std(self.S_ , axis = 1).reshape(-1 ,1))
         
         self.stability_indexes_ = Index
-        self.A_ = np.dot(X , np.linalg.pinv(self.S_))
+        self.A_ = np.dot(X.T , np.linalg.pinv(self.S_))
         
         if plot:
             plt.figure(figsize=(10 , 7))
@@ -285,7 +338,7 @@ def MSTD(X , m , M , step , n_runs , max_iter = 2000 , n_jobs = -1):
 
     Parameters
     ----------
-    X : 2D array, shape (n_samples , n_features)
+    X : 2D array, shape (n_observations , n_variables)
     
     m : int
         minimal dimension for ICA decomposition

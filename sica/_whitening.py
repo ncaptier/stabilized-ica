35d3ce69f036b1d03f2fc2d967b19574a1f511bd
ncaptier <captier.nicolas@gmail.com>
2022-05-16 15:49:31 +0200

    Save mean after whitening

--- sica/_whitening.py
+++ sica/_whitening.py
@@ -6,8 +6,8 @@ from sklearn.utils import check_array, check_random_state
 from sklearn.utils.extmath import svd_flip
 import warnings
 
-"""
-The following code is inspired by the scanpy.tl.pca module (https://scanpy.readthedocs.io/en/stable/api/scanpy.tl.pca.html).
+"""The following code is inspired by the scanpy.tl.pca module (
+https://scanpy.readthedocs.io/en/stable/api/scanpy.tl.pca.html). 
 
 Copyright (c) 2017 F. Alexander Wolf, P. Angerer, Theis Lab
 All rights reserved.
@@ -39,11 +39,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
 
 
 def whitening(
-    X, n_components, svd_solver, chunked, chunk_size, zero_center, random_state=None
+        X, n_components, svd_solver, chunked, chunk_size, zero_center, random_state=None
 ):
-    """ Whiten data (i.e transform variables into a set of new uncorrelated and unit-variance variables) 
-    and reduce dimension trhough a PCA-like approach.
-    This function handles array-like formats as well as sparse matrices.
+    """ Whiten data (i.e. transform variables into a set of new uncorrelated and unit-variance variables) and reduce
+    dimension trhough a PCA-like approach. This function handles array-like formats as well as sparse matrices.
     
     Parameters
     ----------
@@ -54,7 +53,7 @@ def whitening(
         
     svd_solver : str {‘auto’, ‘full’, ‘arpack’, ‘randomized’ , 'lobpcg'}
         solver for the different PCA methods. Please note that some solvers may not be compatible with
-        some of the PCA methods. See PCA, TruncatedSVD and IncrementalPCA from sklearn.decompostion or
+        some PCA methods. See PCA, TruncatedSVD and IncrementalPCA from sklearn.decompostion or
         scipy.sparse.linalg.svds.
         
     chunked : boolean
@@ -75,6 +74,7 @@ def whitening(
     -------
     X_w : 2D ndarray, shape (n_observations , n_components)
 
+    mean : 1D array, shape (n_variables,)
     """
     random_state = check_random_state(random_state)
 
@@ -87,6 +87,7 @@ def whitening(
             n_components=n_components, whiten=True, batch_size=chunk_size
         )
         X_w = pca.fit_transform(X)
+        mean = pca.mean_
 
     elif issparse(X):
 
@@ -105,8 +106,10 @@ def whitening(
             X_w = pca.fit_transform(X)
             X_w = (X_w / pca.singular_values_) * np.sqrt(X.shape[0] - 1)
             X_w -= X_w.mean(axis=0)
+            mean = None
+
         else:
-            X_w = _pca_with_sparse(
+            X_w, mean = _pca_with_sparse(
                 X, n_components, solver=svd_solver, random_state=random_state
             )
 
@@ -114,8 +117,9 @@ def whitening(
 
         pca = PCA(n_components=n_components, whiten=True, svd_solver=svd_solver)
         X_w = pca.fit_transform(X)
+        mean = pca.mean_
 
-    return X_w
+    return X_w, mean
 
 
 def _pca_with_sparse(X, npcs, solver="arpack", mu=None, random_state=None):
@@ -141,7 +145,8 @@ def _pca_with_sparse(X, npcs, solver="arpack", mu=None, random_state=None):
     Returns
     -------
     X_pca : 2D ndarray, shape (n_observations , n_components)
-        
+
+    mu : 1D arrat, shape (n_variables,)
     """
 
     random_state = check_random_state(random_state)
@@ -192,4 +197,4 @@ def _pca_with_sparse(X, npcs, solver="arpack", mu=None, random_state=None):
     # Compute whitened projection (unit-variance and zero mean)
     X_pca = u[:, idx] * np.sqrt(u.shape[0] - 1)
 
-    return X_pca
+    return X_pca, mu

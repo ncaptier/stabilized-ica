d8f56cea8618f6caa1fc2166378aca42307c31a8
ncaptier <captier.nicolas@gmail.com>
2022-05-20 17:33:21 +0200

    Update tests

--- tests/test_StabilizedICA.py
+++ tests/test_StabilizedICA.py
@@ -1,15 +1,17 @@
 import itertools
 import pytest
 
-from sklearn.utils._testing import assert_almost_equal
+from numpy.testing import assert_almost_equal
 
 import numpy as np
 from scipy import stats
 from sklearn.decomposition import PCA
 from sica.base import StabilizedICA
 
+from typing import Optional, Union, Tuple
 
-def center_and_norm(x, axis=-1):
+
+def center_and_norm(x: np.ndarray, axis: Optional[int] = -1) -> None:
     """Centers and norms x **in place**
     Parameters
     -----------
@@ -25,7 +27,13 @@ def center_and_norm(x, axis=-1):
     return
 
 
-def create_data_superGauss(add_noise, seed, N=3, T=1000, M=None):
+def create_data_superGauss(
+        add_noise: bool,
+        seed: int,
+        N: Optional[int] = 3,
+        T: Optional[int] = 1000,
+        M: Optional[Union[int, None]] = None
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.random.RandomState]:
     """ Create a simple data set with superGaussian-distributed sources (Laplace distribution).  
     Parameters
     ----------
@@ -66,7 +74,13 @@ def create_data_superGauss(add_noise, seed, N=3, T=1000, M=None):
     return X, A, S, rng
 
 
-def create_data_subGauss(add_noise, seed, N=3, T=1000, M=None):
+def create_data_subGauss(
+        add_noise: int,
+        seed: int,
+        N: Optional[int] = 3,
+        T: Optional[int] = 1000,
+        M: Optional[Union[int, None]] = None
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.random.RandomState]:
     """ Create a simple data set with subGaussian-distributed sources (uniform distribution).
     """
     rng = np.random.RandomState(seed)
@@ -87,7 +101,13 @@ def create_data_subGauss(add_noise, seed, N=3, T=1000, M=None):
     return X, A, S, rng
 
 
-def create_data_mix(add_noise, seed, N=3, T=1000, M=None):
+def create_data_mix(
+        add_noise: int,
+        seed: int,
+        N: Optional[int] = 3,
+        T: Optional[int] = 1000,
+        M: Optional[Union[int, None]] = None
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.random.RandomState]:
     """ Create a simple data set with a mixture of superGaussian-distributed and 
     subGaussian-distributed sources (Laplace and uniform distributions). 
     """
@@ -111,15 +131,24 @@ def create_data_mix(add_noise, seed, N=3, T=1000, M=None):
     return X, A, S, rng
 
 
-def base_test_simple(strategy, data):
+def base_test_simple(
+        strategy: Tuple[str, str],
+        data: Tuple[np.ndarray, np.ndarray, np.ndarray, np.random.RandomState],
+        noise: bool
+) -> None:
     """ Simple test function for stabilized ICA without resampling.
     Parameters
     ----------
     strategy : (string , string)
         Pair of algorithm and function which characterizes the solver of the ICA problem
         (ex : ('fastica_par' , 'logcosh')).
+
     data : array X, array A, array S, randomstate 
         Simulated data.
+
+    noise : bool
+        Indicate whether the data have been simulated with noise.
+
     Returns
     -------
     None.
@@ -146,19 +175,23 @@ def base_test_simple(strategy, data):
                              verbose=0
                              )
         if whiten:
-            test_A = sica.fit_transform(X.T)
+            test_A = sica.fit_transform(X)
         else:
             pca = PCA(n_components=S.shape[0], whiten=True, random_state=rng)
             Xw = pca.fit_transform(X.T)
-            test_A = sica.fit_transform(Xw)
+            test_Aw = sica.fit_transform(Xw.T)
 
-        assert test_A.shape == A.shape
         assert sica.S_.shape == S.shape
         assert sica.stability_indexes_.shape == (S.shape[0],)
 
         # Check that the mixing model described in the docstring holds:
-        if whiten:
-            assert_almost_equal(X, np.dot(test_A, sica.S_))
+        if not noise:
+            if whiten:
+                assert test_A.shape == A.shape
+                assert_almost_equal(X, np.dot(test_A, sica.S_) + sica.mean_.reshape(-1, 1))
+            else:
+                assert test_Aw.shape == (S.shape[0], S.shape[0])
+                assert_almost_equal(X, pca.inverse_transform(np.dot(test_Aw, sica.S_).T).T)
 
         center_and_norm(sica.S_)
         s1_, s2_, s3_ = sica.S_
@@ -176,12 +209,22 @@ def base_test_simple(strategy, data):
         s3_ *= np.sign(np.dot(s3_, s3))
 
         # Check that we have estimated the original sources
-        assert_almost_equal(np.dot(s1_, s1) / n_samples, 1, decimal=1)
-        assert_almost_equal(np.dot(s2_, s2) / n_samples, 1, decimal=1)
-        assert_almost_equal(np.dot(s3_, s3) / n_samples, 1, decimal=1)
+        if noise:
+            assert_almost_equal(np.dot(s1_, s1) / n_samples, 1, decimal=1)
+            assert_almost_equal(np.dot(s2_, s2) / n_samples, 1, decimal=1)
+            assert_almost_equal(np.dot(s3_, s3) / n_samples, 1, decimal=1)
+        else:
+            assert_almost_equal(np.dot(s1_, s1) / n_samples, 1, decimal=2)
+            assert_almost_equal(np.dot(s2_, s2) / n_samples, 1, decimal=2)
+            assert_almost_equal(np.dot(s3_, s3) / n_samples, 1, decimal=2)
 
 
-def base_test_with_bootstrap(resampling, strategy, data):
+def base_test_with_bootstrap(
+        resampling: str,
+        strategy: Tuple[str, str],
+        data: Tuple[np.ndarray, np.ndarray, np.ndarray, np.random.RandomState],
+        noise: bool
+) -> None:
     """ Simple test function for  StabilizedICA with resampling.
     Parameters
     ----------
@@ -192,6 +235,10 @@ def base_test_with_bootstrap(resampling, strategy, data):
         (ex : ('fastica_par' , 'logcosh')).
     data : array X, array A, array S, randomstate 
         Simulated data.
+
+    noise : bool
+        Indicate whether the data have been simulated with noise.
+
     Returns
     -------
     None.
@@ -210,14 +257,15 @@ def base_test_with_bootstrap(resampling, strategy, data):
         verbose=0,
     )
 
-    test_A = sica.fit_transform(X.T)
+    test_A = sica.fit_transform(X)
 
     assert test_A.shape == A.shape
     assert sica.S_.shape == S.shape
     assert sica.stability_indexes_.shape == (S.shape[0],)
 
     # Check that the mixing model described in the docstring holds:
-    assert_almost_equal(X, np.dot(test_A, sica.S_))
+    if not noise:
+        assert_almost_equal(X, np.dot(test_A, sica.S_) + sica.mean_.reshape(-1, 1))
 
     center_and_norm(sica.S_)
     s1_, s2_, s3_ = sica.S_
@@ -250,7 +298,7 @@ def base_test_with_bootstrap(resampling, strategy, data):
             n_jobs=-1,
             verbose=0,
         )
-        sica_wf.fit(X.T)
+        sica_wf.fit(X)
 
 
 @pytest.mark.parametrize("add_noise", [True, False])
@@ -271,72 +319,72 @@ def base_test_with_bootstrap(resampling, strategy, data):
         ("infomax_ext", "tanh"),
     ],
 )
-def test_StabilizedICA_supGaussian(add_noise, seed, strategy):
-    data = create_data_superGauss(add_noise, seed)
-    base_test_simple(strategy=strategy, data=data)
-
-
-@pytest.mark.parametrize("add_noise", [True, False])
-@pytest.mark.parametrize("seed", range(2))
-@pytest.mark.parametrize(
-    "strategy",
-    [
-        ("fastica_par", "cube"),
-        ("fastica_def", "cube"),
-        ("infomax", "cube"),
-        ("infomax_orth", "cube"),
-        ("fastica_picard", "tanh"),
-        ("infomax_ext", "tanh"),
-    ],
-)
-def test_StabilizedICA_subGaussian(add_noise, seed, strategy):
-    data = create_data_subGauss(add_noise, seed)
-    base_test_simple(strategy=strategy, data=data)
-
-
-@pytest.mark.parametrize("add_noise", [True, False])
-@pytest.mark.parametrize("seed", range(2))
-@pytest.mark.parametrize(
-    "strategy",
-    [
-        ("fastica_par", "cube"),
-        ("fastica_par", "exp"),
-        ("fastica_par", "logcosh"),
-        ("fastica_def", "cube"),
-        ("fastica_def", "exp"),
-        ("fastica_def", "logcosh"),
-        ("fastica_picard", "tanh"),
-        ("infomax_ext", "tanh"),
-    ],
-)
-def test_StabilizedICA_mix(add_noise, seed, strategy):
-    data = create_data_mix(add_noise, seed)
-    base_test_simple(strategy=strategy, data=data)
-
-
-@pytest.mark.parametrize("add_noise", [True, False])
-@pytest.mark.parametrize("seed", range(2))
-@pytest.mark.parametrize(
-    "strategy",
-    [
-        ("fastica_par", "exp"),
-        ("fastica_par", "logcosh"),
-        ("fastica_def", "exp"),
-        ("fastica_def", "logcosh"),
-        ("infomax", "tanh"),
-        ("infomax_orth", "exp"),
-        ("infomax_orth", "tanh"),
-        ("fastica_picard", "exp"),
-        ("fastica_picard", "tanh"),
-        ("infomax_ext", "exp"),
-        ("infomax_ext", "tanh"),
-    ],
-)
-@pytest.mark.parametrize("resampling", ["bootstrap", "fast_bootstrap"])
-@pytest.mark.slow
-def test_StabilizedICA_bootstrap_supGaussian(add_noise, seed, strategy, resampling):
+def test_StabilizedICA_supGaussian(add_noise: bool, seed: bool, strategy: Tuple[str, str]):
     data = create_data_superGauss(add_noise, seed, M=100)
-    base_test_with_bootstrap(resampling=resampling, strategy=strategy, data=data)
+    base_test_simple(strategy=strategy, data=data, noise=add_noise)
+
+
+@pytest.mark.parametrize("add_noise", [True, False])
+@pytest.mark.parametrize("seed", range(2))
+@pytest.mark.parametrize(
+    "strategy",
+    [
+        ("fastica_par", "cube"),
+        ("fastica_def", "cube"),
+        ("infomax", "cube"),
+        ("infomax_orth", "cube"),
+        ("fastica_picard", "tanh"),
+        ("infomax_ext", "tanh"),
+    ],
+)
+def test_StabilizedICA_subGaussian(add_noise: bool, seed: bool, strategy: Tuple[str, str]):
+    data = create_data_subGauss(add_noise, seed, M=10)
+    base_test_simple(strategy=strategy, data=data, noise=add_noise)
+
+
+@pytest.mark.parametrize("add_noise", [True, False])
+@pytest.mark.parametrize("seed", range(2))
+@pytest.mark.parametrize(
+    "strategy",
+    [
+        ("fastica_par", "cube"),
+        ("fastica_par", "exp"),
+        ("fastica_par", "logcosh"),
+        ("fastica_def", "cube"),
+        ("fastica_def", "exp"),
+        ("fastica_def", "logcosh"),
+        ("fastica_picard", "tanh"),
+        ("infomax_ext", "tanh"),
+    ],
+)
+def test_StabilizedICA_mix(add_noise: bool, seed: bool, strategy: Tuple[str, str]):
+    data = create_data_mix(add_noise, seed)
+    base_test_simple(strategy=strategy, data=data, noise=add_noise)
+
+
+@pytest.mark.parametrize("add_noise", [True, False])
+@pytest.mark.parametrize("seed", range(2))
+@pytest.mark.parametrize(
+    "strategy",
+    [
+        ("fastica_par", "exp"),
+        ("fastica_par", "logcosh"),
+        ("fastica_def", "exp"),
+        ("fastica_def", "logcosh"),
+        ("infomax", "tanh"),
+        ("infomax_orth", "exp"),
+        ("infomax_orth", "tanh"),
+        ("fastica_picard", "exp"),
+        ("fastica_picard", "tanh"),
+        ("infomax_ext", "exp"),
+        ("infomax_ext", "tanh"),
+    ],
+)
+@pytest.mark.parametrize("resampling", ["bootstrap", "fast_bootstrap"])
+@pytest.mark.slow
+def test_StabilizedICA_bootstrap_supGaussian(add_noise: bool, seed: bool, strategy: Tuple[str, str], resampling: str):
+    data = create_data_superGauss(add_noise, seed, M=100)
+    base_test_with_bootstrap(resampling=resampling, strategy=strategy, data=data, noise=add_noise)
 
 
 @pytest.mark.parametrize("add_noise", [True, False])
@@ -354,9 +402,9 @@ def test_StabilizedICA_bootstrap_supGaussian(add_noise, seed, strategy, resampli
 )
 @pytest.mark.parametrize("resampling", ["bootstrap", "fast_bootstrap"])
 @pytest.mark.slow
-def test_StabilizedICA_bootstrap_subGaussian(add_noise, seed, strategy, resampling):
+def test_StabilizedICA_bootstrap_subGaussian(add_noise: bool, seed: bool, strategy: Tuple[str, str], resampling: str):
     data = create_data_subGauss(add_noise, seed, M=100)
-    base_test_with_bootstrap(resampling=resampling, strategy=strategy, data=data)
+    base_test_with_bootstrap(resampling=resampling, strategy=strategy, data=data, noise=add_noise)
 
 
 @pytest.mark.parametrize("add_noise", [True, False])
@@ -376,9 +424,9 @@ def test_StabilizedICA_bootstrap_subGaussian(add_noise, seed, strategy, resampli
 )
 @pytest.mark.parametrize("resampling", ["bootstrap", "fast_bootstrap"])
 @pytest.mark.slow
-def test_StabilizedICA_bootstrap_mix(add_noise, seed, strategy, resampling):
+def test_StabilizedICA_bootstrap_mix(add_noise: bool, seed: bool, strategy: Tuple[str, str], resampling: str):
     data = create_data_mix(add_noise, seed, M=100)
-    base_test_with_bootstrap(resampling=resampling, strategy=strategy, data=data)
+    base_test_with_bootstrap(resampling=resampling, strategy=strategy, data=data, noise=add_noise)
 
 
 def test_reorientation():
@@ -386,7 +434,7 @@ def test_reorientation():
     sica = StabilizedICA(
         n_components=3, n_runs=10, reorientation=True, resampling=None, n_jobs=1, verbose=0
     )
-    sica.fit(X.T)
+    sica.fit(X)
 
     assert stats.skew(sica.S_[0, :]) >= 0
     assert stats.skew(sica.S_[1, :]) >= 0
@@ -398,7 +446,7 @@ def test_normalization():
     sica = StabilizedICA(
         n_components=3, n_runs=10, normalize=True, resampling=None, n_jobs=1, verbose=0
     )
-    sica.fit(X.T)
+    sica.fit(X)
 
     assert_almost_equal(np.std(sica.S_[0, :]), 1)
     assert_almost_equal(np.std(sica.S_[1, :]), 1)

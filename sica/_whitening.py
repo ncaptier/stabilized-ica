e7336bbd0ada6c5fa862df27503eb1daed9d6eff
ncaptier <captier.nicolas@gmail.com>
2022-10-26 14:41:45 +0200

    Update _whitening.py

--- sica/_whitening.py
+++ sica/_whitening.py
@@ -1,43 +1,18 @@
+import warnings
+from typing import Optional, Tuple, Union
+
 import numpy as np
-from sklearn.decomposition import PCA, IncrementalPCA, TruncatedSVD
 from scipy.sparse import issparse, spmatrix
 from scipy.sparse.linalg import LinearOperator, svds
+from sklearn.decomposition import PCA, IncrementalPCA, TruncatedSVD
 from sklearn.utils import check_array, check_random_state
 from sklearn.utils.extmath import svd_flip
-import warnings
-
-from typing import Optional, Tuple, Union
 
 """The following code is inspired by the scanpy.tl.pca module (
 https://scanpy.readthedocs.io/en/stable/api/scanpy.tl.pca.html). 
 
 Copyright (c) 2017 F. Alexander Wolf, P. Angerer, Theis Lab
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
+All rights reserved."""
 
 
 def whitening(
